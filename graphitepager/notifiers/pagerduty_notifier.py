09a5066c49e38606e48f217937b2b7980cf3f994
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-06-29 23:20:44 -0400

    Allow overriding service_key on a per-alert type basis

--- graphitepager/notifiers/pagerduty_notifier.py
+++ graphitepager/notifiers/pagerduty_notifier.py
@@ -13,12 +13,25 @@ class PagerdutyNotifier(BaseNotifier):
         self.enabled = config.has_keys(required)
         if self.enabled:
             self._client = pagerduty.PagerDuty(config.get('PAGERDUTY_KEY'))
+            self._pagerduty_config = config.get('pagerduty', {})
+            self._pagerduty_keys = {
+                Level.NOMINAL: self._pagerduty_config.get(
+                    'nominal', self._client.service_key
+                ),
+                Level.WARNING: self._pagerduty_config.get(
+                    'warning', self._client.service_key
+                ),
+                Level.CRITICAL: self._pagerduty_config.get(
+                    'critical', self._client.service_key
+                ),
+                Level.NO_DATA: self._pagerduty_config.get(
+                    'no_data', self._client.service_key
+                ),
+            }
 
     def notify(self, alert, alert_key, level, description, html_description):
-        service_key = None
-        if alert.get('pagerduty_key', None) is not None:
-            service_key = self._client.service_key
-            self._client.service_key = alert.get('pagerduty_key', None)
+        service_key = self._client.service_key
+        self._client.service_key = self._get_service_key(alert, level)
 
         incident_key = self._storage.get_incident_key_for_alert_key(alert_key)
         if level != Level.NOMINAL:
@@ -35,5 +48,8 @@ class PagerdutyNotifier(BaseNotifier):
             self._client.resolve(incident_key=incident_key)
             self._storage.remove_incident_for_alert_key(alert_key)
 
-        if service_key:
-            self._client.service_key = service_key
+        self._client.service_key = service_key
+
+    def _get_service_key(self, alert, level):
+        service_key = self._pagerduty_keys.get(level, self._client.service_key)
+        return alert.get('pagerduty_key', service_key)
